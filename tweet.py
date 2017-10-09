266faced4522a51a80fe0d6754a13427992985b2
Cyrus Sethna <csethna@gmail.com>
2017-10-09 12:43:47 -0500

    create buffer & ids.csv

--- tweet.py
+++ tweet.py
@@ -7,6 +7,12 @@ auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
 api = tweepy.API(auth)
 
+# Create empty list "buffer"
+buffer = []
+
 # Traverse the JSON returned by search query & favorite tweets by ID
 for result in api.search('@codeuptown', 'en'):
-    api.create_favorite(result.id)
+    buffer.append(result.id)
+
+print(buffer)
+    #api.create_favorite(result.id)
