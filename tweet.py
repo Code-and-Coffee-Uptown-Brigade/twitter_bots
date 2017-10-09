83f3039bef2dddfe79e3edaf1f1de4b515ffcedb
Cyrus Sethna <csethna@gmail.com>
2017-10-09 12:20:05 -0500

    Like_bot
    Completed bot which likes statuses mentioning `@codeuptown`

--- tweet.py
+++ tweet.py
@@ -2,34 +2,11 @@ import tweepy
 from credentials import *
 import json
 
-def account_get_tweets():
-    #Get the auth stuff in order
-    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-    auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
-    api = tweepy.API(auth)
+# OAuth to Twitter
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+api = tweepy.API(auth)
 
-    # This function pulls some recent tweets from @codeuptown handle
-    tweets = api.user_timeline(screen_name = 'codeuptown', include_rts= True, parser=tweepy.parsers.JSONParser())
-    return tweets
-
-def mentions():
-    #Get the auth stuff in order
-    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-    auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
-    api = tweepy.API(auth)
-
-    retweets = api.search('@codeuptown', 'en')
-    return retweets
-
-def favorite():
-    #Get the auth stuff in order
-    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-    auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
-    api = tweepy.API(auth)
-
-    favorite = api.create_favorite('917402990646046721')
-    return favorite
-
-#Does this work? Let's find out:
-test = favorite()
-print(test)
+# Traverse the JSON returned by search query & favorite tweets by ID
+for result in api.search('@codeuptown', 'en'):
+    api.create_favorite(result.id)
